0746417633668ff2fe08281ae89da7958d89ba1a
Daniel <dateni24@bemen3.cat>
2025-11-17 20:48:10 +0100

    ejercicio 7 finalizado!

--- lib/cat/bemen/dam/0488/ra2/p2/exercicis/E7/provider_counter/lib/main_7.dart
+++ lib/cat/bemen/dam/0488/ra2/p2/exercicis/E7/provider_counter/lib/main_7.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider_counter/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_counter/providers/counter_provider.dart';
 
 
-void main() => runApp(const MyApp());
+
+void main() {
+runApp(ChangeNotifierProvider(
+  create: (_) => CounterProvider(),
+  child: const MyApp(), ));
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
