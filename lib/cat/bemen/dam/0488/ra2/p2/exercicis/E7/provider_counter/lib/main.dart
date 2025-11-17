c48372d91e846d80d475c6d88968967367f5527c
TDano245 <danicr24@bemen3.cat>
2025-11-17 19:18:09 +0100

    parte2.2

---
+++ lib/cat/bemen/dam/0488/ra2/p2/exercicis/E7/provider_counter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Provider Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
